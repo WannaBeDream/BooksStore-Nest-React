1e0aed4c40c32a4ae07ea88390c755d19bd7b811
Alex <46503458+WannaBeDream@users.noreply.github.com>
2019-08-23 11:25:57 +0300

    added  helmet, rateLimit, enableCors

--- backend-book-shop/src/main.ts
+++ backend-book-shop/src/main.ts
@@ -1,21 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
 import * as helmet from 'helmet'; 
-import * as csurf from 'csurf';
+// import * as csurf from 'csurf';    // TODO
 import * as rateLimit from 'express-rate-limit'; // somewhere in my initialization file
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(helmet()); // Armor headers
-//   app.enableCors(); // CORS
-//   app.use(csurf()); // CSRF armor
-//   app.use(
-//   rateLimit({
-//     windowMs: 15 * 60 * 1000, // 15 minutes
-//     max: 100, // limit each IP to 100 requests per windowMs
-//   }),
-// );
+  app.use(helmet()); // Armor headers
+  app.enableCors(); // CORS
+  // app.use(csurf()); // CSRF armor           //TODO
+  app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }),
+);
   await app.listen(3000);
   
 }
